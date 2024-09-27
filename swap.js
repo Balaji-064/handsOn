ba398b94d1593cb64dc687ebc4e4369e3166ec59
Balaji-064 <praveenbalaji433@gmail.com>
2024-09-27 16:32:27 +0530

    swap of number

---
+++ swap.js
@@ -0,0 +1,6 @@
+var a = 5;
+var b = 11;
+a = a + b;
+b = a - b;
+a = a - b;
+console.log(a, b);
---
+++ swap.ts
@@ -0,0 +1,8 @@
+/*Declare two variables x and y with numerical values. Swap their values without using a third variable*/
+
+let a:number=5;
+let b:number=11;
+a=a+b;
+b=a-b;
+a=a-b;
+console.log(a,b);
