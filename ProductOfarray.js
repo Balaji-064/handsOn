6bbe410f88c1c80e63b2949d60f01eec32ce4162
Balaji-064 <praveenbalaji433@gmail.com>
2025-01-20 17:06:55 +0530

    20-01-2025 coding challenges

---
+++ ProductOfarray.js
@@ -0,0 +1,4 @@
+var a = [1, 3, 4, 9, 7];
+var b = a.sort(function (a, b) { return b - a; });
+console.log(b[0] * b[1]);
+console.log(b);
---
+++ ProductOfarray.ts
@@ -0,0 +1,5 @@
+var givenArray=[1,3,4,9,7]
+var SortedArray=givenArray.sort((a,b)=>b-a)
+console.log(SortedArray[0]*SortedArray[1])
+
+console.log(SortedArray)
